e57e174c24727f95664422e2b0446bc02b21f24a
Kevin Nijmeijer <kevin.nijmeijer@student.hu.nl>
2014-02-10 16:38:49 +0100

    motortest

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
+
 
 public class Main {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println("ROBOT DESUKA");
-
+		// TODO Auto-generated method stub
+		//stuur
+		NXTRegulatedMotor motorA = Motor.getInstance(0);
+		NXTRegulatedMotor motorB = Motor.getInstance(1);
+		
+		motorA.setSpeed(100);
+		motorB.setSpeed(100);
+		
+		while(true){
+			motorA.forward();
+			motorB.forward();
+		}
 	}
-
 }
